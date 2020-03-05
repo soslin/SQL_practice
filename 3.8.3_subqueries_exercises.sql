c90fa844e7df3c9211c4b865ee34076f1f37e775
soslin <seanoslin@gmail.com>
2020-03-05 10:21:25 -0600

    Final commit

--- 3.8.3_subqueries_exercises.sql
+++ 3.8.3_subqueries_exercises.sql
@@ -29,12 +29,36 @@ WHERE emp_no IN (
 GROUP BY title;
 
 # How many people in the employees table are no longer working for the company?
-
-
 SELECT COUNT(emp_no)
 FROM employees
 WHERE emp_no NOT IN (
 	SELECT emp_no
     FROM dept_emp
     WHERE to_date = '9999-01-01');
+
+# Find all the current department managers that are female.
+SELECT first_name, last_name, gender
+FROM employees
+WHERE emp_no IN (
+	SELECT emp_no
+    FROM dept_manager
+    WHERE to_date = '9999-01-01'
+    ) AND gender = 'F';
+
+# Find all the employees that currently have a higher than average salary.
+
+SELECT e.first_name, e.last_name, s.salary
+FROM employees AS e
+JOIN salaries AS s
+	ON (e.emp_no = s.emp_no)
+WHERE s.salary > (
+	SELECT AVG(salary)
+    FROM salaries)
+AND s.to_date = '9999-01-01';
+
+
+
+
+
+
 
